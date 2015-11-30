5f30d58cb11ddddbe1add09c5a9a2c4ee17c2e26
Raphael <rbabel@yes-we-code.fr>
2015-11-30 09:26:13 +0100

    composer.json

---
+++ Remote/Entity.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace YWC\Common\RemoteBundle\Remote;
+
+class Entity
+{
+    
+    protected $id;
+
+    private $remoteEntityUrl;
+
+    private $remoteEntityLoaded = false;
+
+    private $remoteEntityType;
+
+    // array of: array('attribute' => 'str', 'repository' => 'str')
+    protected $remoteEntityAttached = array();
+
+    protected $remoteEntityPrivateAttributes = array(
+        'remoteEntityUrl',
+        'remoteEntityType',
+        'remoteEntityLoaded',
+        'remoteEntityAttributes',
+        'remoteEntityPrivateAttributes',
+        'remoteEntityAttached',
+        'id',
+    );
+
+    private $remoteEntityAttributes = array();
+
+    public function __construct($url, $id)
+    {
+        $this->id = $id;
+        $tmp = explode('\\', get_class($this));
+        $this->remoteEntityType = strtolower(array_pop($tmp));
+        $this->remoteEntityUrl = $url;
+        foreach($this as $key => $value) {
+            if(!in_array($key, $this->remoteEntityPrivateAttributes)) $this->remoteEntityAttributes[] = $key;
+        }
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    private function getUrl()
+    {
+        return $this->remoteEntityUrl.$this->remoteEntityType.'/'.$this->getId();
+    }
+
+    public function getApi()
+    {
+        return substr($this->remoteEntityUrl, 0, -7);
+    }
+
+    protected function load()
+    {
+        $this->fromJson(json_decode(file_get_contents($this->getUrl())));        
+    }
+
+    public function fromJson($json)
+    {
+        $ret = $this->remoteEntityType;
+        foreach($this->remoteEntityAttributes as $key) {            
+            $this->$key = $json->$ret->$key;
+        }
+        $this->remoteEntityLoaded = true;
+    }
+
+    public function remoteEntityGetAttached()
+    {
+        return $this->remoteEntityAttached;
+    }
+
+    public function  __call($name, $args)
+    {
+        if(strpos($name, 'get') === 0) {
+            $attr = lcfirst(substr($name, 3));
+            if(!in_array($attr, $this->remoteEntityAttributes)) trigger_error('Call to undefined method '.__CLASS__.'::'.$name.'()', E_USER_ERROR);
+            if(!$this->remoteEntityLoaded) $this->load();
+        
+            return $this->$attr;
+        }
+        elseif(strpos($name, 'attach') === 0) {
+            $attr = lcfirst(substr($name, 6));
+            if(!in_array($attr, array_map(function ($item) {
+                return $item['attribute'];
+            }, $this->remoteEntityAttached))) trigger_error('Call to undefined method '.__CLASS__.'::'.$name.'()', E_USER_ERROR);
+            $this->$attr = $args[0];
+        }
+        else trigger_error('Call to undefined method '.__CLASS__.'::'.$name.'()', E_USER_ERROR);
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+    
+}
---
+++ Remote/RemoteEntityManager.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace YWC\Common\RemoteBundle\Remote;
+
+use Doctrine\ORM\EntityManager;
+
+class RemoteEntityManager
+{
+
+    private $url;
+
+    private $em;
+
+    private $loaded = array();
+
+    public function __construct($url, EntityManager $em)
+    {
+        $this->url = $url.'entity/';
+        $this->em = $em;
+    }
+
+    public function setRemote($url)
+    {
+        $this->url = $url.'entity/';
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function getApi()
+    {
+        return substr($this->url, 0, -7);
+    }
+
+    public function get($type, $id = null, $url = null)
+    {
+        $type = strtolower($type);
+
+        if(is_null($id) && !is_null($url)) {
+
+            return $this->loaded[$type][$url];
+        }
+
+        if(is_null($id)) {
+            $result = array();
+            if(isset($this->loaded[$type])) {
+                foreach($this->loaded[$type] as $urls) {
+                    foreach($urls as $item) $result[] = $item;
+                }
+            }
+
+            return $result;
+        }
+
+        if(is_null($url)) {
+            if(!isset($this->loaded[$type][$this->url][$id])) {        
+                $reflect = new \ReflectionClass('YWC\CommonBundle\Remote\Entity\\'.ucfirst($type));        
+                $this->loaded[$type][$this->url][$id] = $reflect->newInstanceArgs(array($this->url, $id));
+            }
+
+            return $this->loaded[$type][$this->url][$id];
+        }
+    }
+
+    public function jsonLoad($type, $json)
+    {
+        $type = strtolower($type);
+        $json = json_decode($json)->{$type.'s'};
+        $reflect = new \ReflectionClass('YWC\CommonBundle\Remote\Entity\\'.ucfirst($type));
+        foreach($json as $item) {
+            if(!isset($this->loaded[$type][$this->url][$item->id])) {                
+                $item = (object) array($type => $item);
+                $newItem = $reflect->newInstanceArgs(array($this->url, $item->$type->id));
+                $newItem->fromJson($item);
+                $this->loaded[$type][$this->url][$item->$type->id] = $newItem;
+            }
+        }
+    }
+
+    public function attachLocalEntities($remoteEntity)
+    {
+        foreach($remoteEntity->remoteEntityGetAttached() as $entity) {
+            $localEntity = $this->em->getRepository($entity['repository'])->find(call_user_func_array(array($remoteEntity, 'get'.ucfirst($entity['attribute'])), array()));
+            call_user_func_array(array($remoteEntity, 'attach'.ucfirst($entity['attribute'])), array($localEntity));
+        }
+    }
+}
