f7453bad38882d069e71576484f7fd844a1f4ba7
Raphael <rbabel@yes-we-code.fr>
2015-11-30 14:45:53 +0100

    remote entities

---
+++ Remote/Entity/Project.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace YWC\CommonBundle\Remote\Entity;
+
+use YWC\API\AdminBundle\Entity\Client;
+use YWC\CommonBundle\Remote\Entity;
+
+class Project extends Entity
+{
+    protected $name;
+
+    protected $trade;
+
+    protected $geoloc;
+
+    protected $geolocRadius;
+
+    protected $client;
+
+    protected $strict;
+
+    protected $user;
+
+    protected $CV;
+
+    protected $LDM;
+
+    public function __construct($url, $id)
+    {
+        $this->remoteEntityPrivateAttributes[] = 'client';
+        parent::__construct($url, $id);
+    }
+
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getGeoloc()
+    {
+        return $this->geoloc;
+    }
+
+    public function setGeoloc($geoloc)
+    {
+        $this->geoloc = $geoloc;
+
+        return $this;
+    }
+
+    public function getCV()
+    {
+        $this->load();
+        
+        return $this->CV;
+    }
+
+    public function getLDM()
+    {
+        $this->load();
+        
+        return $this->LDM;
+    }
+
+    public function isValid()
+    {        
+        return $this->getCV() && $this->getLDM();
+    }    
+        
+}
