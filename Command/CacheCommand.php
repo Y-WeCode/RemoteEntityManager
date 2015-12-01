a052d0b24204ecbf56a3361c4d216753fb82952e
Raphael <rbabel@yes-we-code.fr>
2015-12-01 10:01:40 +0100

    typo

--- Command/CacheCommand.php
+++ Command/CacheCommand.php
@@ -4,7 +4,7 @@ namespace YWC\RemoteBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Intput\InputArgument;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
